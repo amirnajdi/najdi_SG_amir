1cc6319642b3dc1100bb992624372aa6661b63f3
Amir Najdi <amirnajdi137980@gmail.com>
2023-05-23 20:03:55 +0330

    feat: add helper for manage http response

---
+++ src/Helpers/Response.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Helpers;
+
+enum ResponseStatus: string
+{
+    case SUCCESS = 'success';
+    case SERVER_ERROR = 'Server Error';
+}
+
+enum HTTPStatusCode: int
+{
+    case SUCCESS = 200;
+    case SERVER_ERROR = 500;
+}
+
+class Response
+{
+
+    private array $data = [];
+    private ResponseStatus $status = ResponseStatus::SUCCESS;
+
+    public function setData(string $key, mixed $data)
+    {
+        $this->data[$key] = $data;
+        return $this;
+    }
+
+    public function setStatus(ResponseStatus $status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function setHTTPStatusCode(HTTPStatusCode $statusCode)
+    {
+        http_response_code($statusCode->value);
+        return $this;
+    }
+
+    public function sendAsJson()
+    {
+        echo json_encode([
+            'status' => $this->status->value,
+            'data' => $this->data
+        ]);
+    }
+}
