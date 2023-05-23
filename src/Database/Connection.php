0814bf391d0d8d1b33066f512a4c0bcd3a6f7678
Amir Najdi <amirnajdi137980@gmail.com>
2023-05-23 18:22:28 +0330

    fix: change PDO error level to emit an exception
    change PDO::ERRMODE_SILENT to PDO::ERRMODE_EXCEPTION

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -32,7 +32,7 @@ class Connection
 
         try {
             self::$instance = new PDO("mysql:host=" . $db_info['db_host'] . ';port=' . $db_info['db_port'] . ';dbname=' . $db_info['db_name'], $db_info['db_user'], $db_info['db_pass']);
-            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$instance->exec(
                 "SET NAMES 'utf8';
                 SET character_set_connection=utf8;
