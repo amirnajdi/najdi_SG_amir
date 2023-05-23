b2e59dc7e58bb4f214a1c8f6b7b14b472ee25a26
Amir Najdi <amirnajdi137980@gmail.com>
2023-05-23 11:37:16 +0330

    chore(env): install and config vlucas/phpdotenv v5.5

--- src/index.php
+++ src/index.php
@@ -3,3 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/routes.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+$dotenv->required(['DB_HOST', 'DB_PORT', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD'])
+    ->notEmpty();
