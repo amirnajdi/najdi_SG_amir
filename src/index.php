b46f6144ec746f85166b1ad894cfeea2e5dae74f
Amir Najdi <amirnajdi137980@gmail.com>
2023-05-23 18:11:19 +0330

    fix: load .env file before the router

--- src/index.php
+++ src/index.php
@@ -1,9 +1,10 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/routes.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 $dotenv->required(['DB_HOST', 'DB_PORT', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD'])
     ->notEmpty();
+
+require __DIR__ . '/routes.php';
