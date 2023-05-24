d0ab62dcde1c4462b6b123bc3e81081a5a8ca010
Amir Najdi <amirnajdi137980@gmail.com>
2023-05-24 15:52:28 +0330

    refactor: mange some duplicated code

--- src/Controllers/ItemsController.php
+++ src/Controllers/ItemsController.php
@@ -21,11 +21,7 @@ class ItemsController
     public function show(string $uuid)
     {
         $item = (new Items())->findByUuid($uuid);
-        if ($item == []) {
-            $response = (new Response())->setHTTPStatusCode(HTTPStatusCode::NOT_FOUND)
-                ->setStatus(ResponseStatus::NOT_FOUND);
-            return $response->sendAsJson();
-        }
+        if ($item == []) return $this->itemNotFound();
 
         $response = (new Response())->setData('item', $item);
         $response->sendAsJson();
@@ -34,12 +30,7 @@ class ItemsController
     public function create()
     {
         $title = htmlspecialchars(trim($_POST['title']));
-        if ($title == null) {
-            $response = (new Response())->setHTTPStatusCode(HTTPStatusCode::UNPROCESSABLE_ENTITY)
-                ->setStatus(ResponseStatus::UNPROCESSABLE_ENTITY)
-                ->setMessage('title is a required field!');
-            return $response->sendAsJson();
-        }
+        if ($title == null) return $this->titleIsRequired();
 
         $item = (new Items)->insertOneItem($title);
 
@@ -52,24 +43,15 @@ class ItemsController
 
     public function edit(string $uuid)
     {
-        parse_str(file_get_contents("php://input"),$data);
+        parse_str(file_get_contents("php://input"), $data);
         $title = htmlspecialchars(trim($data['title']));
-        if ($title == null) {
-            $response = (new Response())->setHTTPStatusCode(HTTPStatusCode::UNPROCESSABLE_ENTITY)
-                ->setStatus(ResponseStatus::UNPROCESSABLE_ENTITY)
-                ->setMessage('title is a required field!');
-            return $response->sendAsJson();
-        }
-        
+        if ($title == null) return $this->titleIsRequired();
+
         $response = new Response();
         $itemInstane = new Items();
+
         $item = $itemInstane->findByUuid($uuid);
-        if ($item == []) {
-            return $response->setHTTPStatusCode(HTTPStatusCode::NOT_FOUND)
-                ->setStatus(ResponseStatus::NOT_FOUND)
-                ->setMessage('The item was not found!')
-                ->sendAsJson();
-        }
+        if ($item == []) return $this->itemNotFound();
 
         $deleteStatus = $itemInstane->update($uuid, $title);
         if (!$deleteStatus) {
@@ -87,13 +69,9 @@ class ItemsController
     {
         $response = new Response();
         $itemInstane = new Items();
+        
         $item = $itemInstane->findByUuid($uuid);
-        if ($item == []) {
-            return $response->setHTTPStatusCode(HTTPStatusCode::NOT_FOUND)
-                ->setStatus(ResponseStatus::NOT_FOUND)
-                ->setMessage('The item was not found!')
-                ->sendAsJson();
-        }
+        if ($item == []) return $this->itemNotFound();
 
         $deleteStatus = $itemInstane->deleteItemByUuid($uuid);
         if (!$deleteStatus) {
@@ -111,17 +89,13 @@ class ItemsController
     {
         $response = new Response();
         $itemInstane = new Items();
+
         $item = $itemInstane->findByUuid($uuid);
-        if ($item == []) {
-            return $response->setHTTPStatusCode(HTTPStatusCode::NOT_FOUND)
-                ->setStatus(ResponseStatus::NOT_FOUND)
-                ->setMessage('The item was not found!')
-                ->sendAsJson();
-        }
+        if ($item == []) return $this->itemNotFound();
 
         // if item is_done_at is null, it means the item is not done yet and we change the is_done_at to the current date,
         // and if is_done_at is not null it means is done and we change to null
-        $changeStatusTo = $item['is_done_at'] == null ? true: false;
+        $changeStatusTo = $item['is_done_at'] == null ? true : false;
 
         $deleteStatus = $itemInstane->updateStatus($uuid, $changeStatusTo);
         if (!$deleteStatus) {
@@ -132,6 +106,22 @@ class ItemsController
         }
 
         $response->setMessage("The shopping list item status was updated successfully")
+            ->sendAsJson();
+    }
+
+    private function titleIsRequired()
+    {
+        return (new Response())->setHTTPStatusCode(HTTPStatusCode::UNPROCESSABLE_ENTITY)
+            ->setStatus(ResponseStatus::UNPROCESSABLE_ENTITY)
+            ->setMessage('title is a required field!')
+            ->sendAsJson();
+    }
+
+    private function itemNotFound()
+    {
+        return (new Response())->setHTTPStatusCode(HTTPStatusCode::NOT_FOUND)
+            ->setStatus(ResponseStatus::NOT_FOUND)
+            ->setMessage('The item was not found!')
             ->sendAsJson();
     }
 }
