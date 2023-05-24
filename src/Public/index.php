480385e4340c04d8e77589bb0c1390ecd9607a1b
Amir Najdi <amirnajdi137980@gmail.com>
2023-05-24 12:37:42 +0330

    chore: move .env file to root directory

--- src/Public/index.php
+++ src/Public/index.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../../");
 $dotenv->safeLoad();
 $dotenv->required(['DB_HOST', 'DB_PORT', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD'])
     ->notEmpty();
