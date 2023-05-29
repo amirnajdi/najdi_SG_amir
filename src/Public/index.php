f735092593570e63d6981a9da66f791b41adcc0d
Amir Najdi <amirnajdi@outlook.com>
2023-05-29 14:39:33 +0330

    chore(JWT): install and config firebase/php-jwt v6.5

--- src/Public/index.php
+++ src/Public/index.php
@@ -2,9 +2,11 @@
 
 require __DIR__ . '/../../vendor/autoload.php';
 
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../../");
 $dotenv->safeLoad();
-$dotenv->required(['DB_HOST', 'DB_PORT', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD'])
+$dotenv->required(['DB_HOST', 'DB_PORT', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD', 'JWT_KEY', 'APP_DOMAIN'])
     ->notEmpty();
+$dotenv->ifPresent(['JWT_TOKEN_LIFE_TIME', 'DB_PORT'])->isInteger();
 
 require __DIR__ . '/../routes.php';
